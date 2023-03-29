3c8335355f1a53a2ba207daa47d24395fc271be9
Devin Frandsen <devinfrandsen@gmail.com>
2023-03-29 14:15:16 -0600

    Finished Database Class Skeleton

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const database = require('./database.js');
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
@@ -6,6 +7,21 @@ app.use(express.json());
 app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use('/api', apiRouter);
+
+apiRouter.post('/auth/createUser', async (req, res) => {
+    if (await database.getUser(req.body.username)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await DB.createUser(req.body.username, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
+    }
+  });
 
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
